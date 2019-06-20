6eaef017f3d3a942a57c530ac72d119e6bd449ea
jorgenbraseth <jorgen.braseth@unacast.com>
2019-06-20 19:18:45 +0200

    Backend functionally done

---
+++ backend/src/repo.mjs
@@ -0,0 +1,152 @@
+import db from 'sqlite';
+import Promise from 'bluebird';
+
+const DB = {
+    init: () => {
+        return Promise.resolve()
+        // First, try to open the database
+            .then(() => db.open('./database.sqlite', {Promise}))      // <=
+            // Update db schema to the latest version using SQL-based migrations
+            .then(() => db.migrate({force: 'last'}))                  // <=
+            // Display error message if something went wrong
+            .then(() => db.run("DELETE FROM Shapes"))
+            .then(() => SHAPES.features.forEach((feature) => {
+                db.run(`INSERT INTO Shapes(geojson) VALUES(?)`, [JSON.stringify(feature)])
+            }))
+            .catch((err) => console.error(err.stack));
+    },
+    getAll: () => {
+        return db.all("SELECT * FROM Shapes", []).then((rows) => {
+            return rows.map((row) => {
+                let geo = JSON.parse(row.geojson);
+                geo.properties.id = row.id;
+                return geo;
+            })
+        })
+    },
+    get: (id) => {
+        return db.get("SELECT * FROM Shapes WHERE id = ?", id).then((row) => {
+            let geo = JSON.parse(row.geojson);
+            geo.properties.id = row.id;
+            return geo;
+        })
+    },
+
+    replace: (ids, replacement) => {
+        let deletes = [];
+        ids.forEach((toDelete) => {
+            console.log(ids);
+            console.log(toDelete);
+            deletes.push(db.run('DELETE FROM SHAPES WHERE id=?', toDelete));
+        });
+        delete replacement.properties.id;
+        const jsonString = JSON.stringify(replacement);
+        console.log(jsonString);
+
+        return Promise.all(deletes).then(() => {
+            return db.run('INSERT INTO SHAPES( geojson ) VALUES ( ? )', [jsonString]);
+        });
+    }
+};
+
+export default DB;
+
+
+const SHAPES = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [
+                    [
+                        [
+                            -0.14007568359375,
+                            51.5027589576403
+                        ],
+                        [
+                            -0.12325286865234374,
+                            51.5027589576403
+                        ],
+                        [
+                            -0.12325286865234374,
+                            51.512588580360244
+                        ],
+                        [
+                            -0.14007568359375,
+                            51.512588580360244
+                        ],
+                        [
+                            -0.14007568359375,
+                            51.5027589576403
+                        ]
+                    ]
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [
+                    [
+                        [
+                            -0.1352691650390625,
+                            51.50810140697543
+                        ],
+                        [
+                            -0.11398315429687499,
+                            51.50810140697543
+                        ],
+                        [
+                            -0.11398315429687499,
+                            51.51963895991333
+                        ],
+                        [
+                            -0.1352691650390625,
+                            51.51963895991333
+                        ],
+                        [
+                            -0.1352691650390625,
+                            51.50810140697543
+                        ]
+                    ]
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [
+                    [
+                        [
+                            -0.13595581054687497,
+                            51.49698840879303
+                        ],
+                        [
+                            -0.11226654052734375,
+                            51.49698840879303
+                        ],
+                        [
+                            -0.11226654052734375,
+                            51.50510971251776
+                        ],
+                        [
+                            -0.13595581054687497,
+                            51.50510971251776
+                        ],
+                        [
+                            -0.13595581054687497,
+                            51.49698840879303
+                        ]
+                    ]
+                ]
+            }
+        }
+    ]
+};
---
+++ backend/src/server.mjs
@@ -0,0 +1,54 @@
+import express from 'express';
+import DB from './repo.mjs';
+import Promise from 'bluebird';
+import bodyParser from 'body-parser';
+import Turf from 'turf';
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+const port = process.env.PORT || 3000;
+
+app.get('/shapes', async (req, res, next) => {
+    try {
+        // const shapes = await db.all('SELECT * FROM Shapes LIMIT 10'); // <=
+        DB.getAll().then((body) => res.send(body));
+    } catch (err) {
+        next(err);
+    }
+});
+
+app.post('/_union', async (req, res, next) => {
+    try {
+        const [f1, f2] = await Promise.all(req.body.shapes.map(s => DB.get(s)));
+        let u = Turf.union(f1, f2);
+        await DB.replace(req.body.shapes, u);
+        res.send(u);
+    } catch (err) {
+        next(err);
+    }
+});
+
+app.post('/_intersect', async (req, res, next) => {
+    try {
+        const [f1, f2] = await Promise.all(req.body.shapes.map(s => DB.get(s)));
+        let u = Turf.intersect(f1, f2);
+        await DB.replace(req.body.shapes, u);
+        res.send(u);
+    } catch (err) {
+        next(err);
+    }
+});
+
+Promise.resolve()
+    .then(() => DB.init())
+    .catch((err) => console.error(err.stack))
+    .finally(() => app.listen(port));
+
+
